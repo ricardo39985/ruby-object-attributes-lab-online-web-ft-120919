5e957cf0527ac0509a5d9085402daaeb43d54109
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-16 21:51:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
   def name=(name)
-    @name=name    
+    @name=name
   end
   def job=(job)
-    @job=job    
+    @job=job
   end
   def name
-    @name    
+    @name
   end
   def job
-    @job    
+    @job
   end
 end
